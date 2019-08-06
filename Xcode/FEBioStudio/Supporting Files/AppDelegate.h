4cb2b28fc73cf0a89685cb0a80867b497863f5ed
Gerard Ateshian <ateshian@columbia.edu>
2019-08-06 23:44:28 +0000

    FEBio Studio: updated Xcode project
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBioStudio@12425 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ Xcode/FEBioStudio/Supporting Files/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FEBioStudio
+//
+//  Created by Gerard Ateshian on 8/6/19.
+//  Copyright © 2019 febio.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
