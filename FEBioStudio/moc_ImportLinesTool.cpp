dc761cbbce37885c79d6c333e8c7b7420ff42073
Steve Maas <steve.maas@utah.edu>
2019-12-09 16:52:39 +0000

    FEBioStudio: forgot to commit new moc files.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBioStudio@12907 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ FEBioStudio/moc_ImportLinesTool.cpp
@@ -0,0 +1,220 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ImportLinesTool.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "ImportLinesTool.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ImportLinesTool.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CImportLinesTool_t {
+    QByteArrayData data[4];
+    char stringdata0[35];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CImportLinesTool_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CImportLinesTool_t qt_meta_stringdata_CImportLinesTool = {
+    {
+QT_MOC_LITERAL(0, 0, 16), // "CImportLinesTool"
+QT_MOC_LITERAL(1, 17, 7), // "OnApply"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 8) // "OnBrowse"
+
+    },
+    "CImportLinesTool\0OnApply\0\0OnBrowse"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CImportLinesTool[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x08 /* Private */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CImportLinesTool::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CImportLinesTool *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->OnApply(); break;
+        case 1: _t->OnBrowse(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject CImportLinesTool::staticMetaObject = { {
+    &CAbstractTool::staticMetaObject,
+    qt_meta_stringdata_CImportLinesTool.data,
+    qt_meta_data_CImportLinesTool,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *CImportLinesTool::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CImportLinesTool::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CImportLinesTool.stringdata0))
+        return static_cast<void*>(this);
+    return CAbstractTool::qt_metacast(_clname);
+}
+
+int CImportLinesTool::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = CAbstractTool::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+struct qt_meta_stringdata_CImportPointsTool_t {
+    QByteArrayData data[4];
+    char stringdata0[36];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CImportPointsTool_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CImportPointsTool_t qt_meta_stringdata_CImportPointsTool = {
+    {
+QT_MOC_LITERAL(0, 0, 17), // "CImportPointsTool"
+QT_MOC_LITERAL(1, 18, 7), // "OnApply"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 8) // "OnBrowse"
+
+    },
+    "CImportPointsTool\0OnApply\0\0OnBrowse"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CImportPointsTool[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x08 /* Private */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CImportPointsTool::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CImportPointsTool *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->OnApply(); break;
+        case 1: _t->OnBrowse(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject CImportPointsTool::staticMetaObject = { {
+    &CAbstractTool::staticMetaObject,
+    qt_meta_stringdata_CImportPointsTool.data,
+    qt_meta_data_CImportPointsTool,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *CImportPointsTool::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CImportPointsTool::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CImportPointsTool.stringdata0))
+        return static_cast<void*>(this);
+    return CAbstractTool::qt_metacast(_clname);
+}
+
+int CImportPointsTool::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = CAbstractTool::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
