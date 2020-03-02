9b031fa25f22722e19d1b7fd238e5511e32ef48d
Steve Maas <steve.maas@utah.edu>
2020-03-02 22:44:10 +0000

    FEBioStudio: Added descriptive enums to FENodalLoad.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBioStudio@13103 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- FEMLib/FELoad.cpp
+++ FEMLib/FELoad.cpp
@@ -7,7 +7,7 @@
 FENodalLoad::FENodalLoad(FEModel* ps) : FELoad(FE_NODAL_LOAD, ps)
 {
 	SetTypeString("Nodal Load");
-	AddIntParam(0, "bc", "bc");
+	AddIntParam(0, "bc", "bc")->SetEnumNames("x-force\0y-force\0z-force\0");
 	AddDoubleParam(1, "load", "load")->SetLoadCurve();
 }
 
@@ -15,6 +15,6 @@ FENodalLoad::FENodalLoad(FEModel* ps) : FELoad(FE_NODAL_LOAD, ps)
 FENodalLoad::FENodalLoad(FEModel* ps, FEItemListBuilder* pi, int bc, double f, int nstep) : FELoad(FE_NODAL_LOAD, ps, pi, nstep)
 {
 	SetTypeString("Nodal Load");
-	AddIntParam(bc, "bc", "bc");
+	AddIntParam(bc, "bc", "bc")->SetEnumNames("x-force\0y-force\0z-force\0");
 	AddDoubleParam(f, "load", "load")->SetLoadCurve();
 }
