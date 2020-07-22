92ec4e983f12489dd7c7e433afd7c750cf15d711
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2020-07-22 16:44:55 -0600

    Fixed issue with wrong object rendering functions called.

--- PostGL/GLModel.cpp
+++ PostGL/GLModel.cpp
@@ -1930,9 +1930,11 @@ void CGLModel::RenderObjects(CGLContext& rc)
 		{
 		case 1: glx::renderRigidBody(R); break;
 		case 2: glx::renderJoint(R); break;
-		case 3: glx::renderPrismaticJoint(R); break;
-		case 4: glx::renderRevoluteJoint(R); break;
-		case 5: glx::renderCylindricalJoint(R); break;
+		case 3: glx::renderJoint(R); break;
+		case 4: glx::renderPrismaticJoint(R); break;
+		case 5: glx::renderRevoluteJoint(R); break;
+		case 6: glx::renderCylindricalJoint(R); break;
+		case 7: glx::renderPlanarJoint(R); break;
 		default:
 			glx::renderAxis(R);
 		}
