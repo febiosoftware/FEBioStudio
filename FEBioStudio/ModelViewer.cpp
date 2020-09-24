ccbde53745f58f088d3fea676cc3f92fb31a79c4
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2020-09-24 14:24:01 -0600

    Added option to convert part selection into element selection.

--- FEBioStudio/ModelViewer.cpp
+++ FEBioStudio/ModelViewer.cpp
@@ -784,6 +784,43 @@ void CModelViewer::OnHidePart()
 	wnd->RedrawGL();
 }
 
+void CModelViewer::OnSelectPartElements()
+{
+	CModelDocument* doc = dynamic_cast<CModelDocument*>(GetDocument());
+	GModel& m = doc->GetFEModel()->GetModel();
+
+	if (m_selection.size() != 1) return;
+	GPart* pg = dynamic_cast<GPart*>(m_selection[0]); assert(pg);
+
+	GObject* po = dynamic_cast<GObject*>(pg->Object());
+	if (po == nullptr) return;
+
+	FEMesh* pm = po->GetFEMesh();
+	if (pm == nullptr) return;
+
+	// set the correct selection mode
+	doc->SetSelectionMode(SELECT_OBJECT);
+	doc->SetItemMode(ITEM_ELEM);
+
+	// make sure this object is selected first
+	doc->DoCommand(new CCmdSelectObject(&m, po, false));
+
+	// now, select the elements
+	int lid = pg->GetLocalID();
+	vector<int> elemList;
+	for (int i = 0; i < pm->Elements(); ++i)
+	{
+		FEElement& el = pm->Element(i);
+		if (el.m_gid == lid) elemList.push_back(i);
+	}
+
+	// select elements
+	doc->DoCommand(new CCmdSelectElements(pm, elemList, false));
+
+	CMainWindow* wnd = GetMainWindow();
+	wnd->RedrawGL();
+}
+
 void CModelViewer::OnShowPart()
 {
 	CModelDocument* doc = dynamic_cast<CModelDocument*>(GetDocument());
@@ -1477,6 +1514,7 @@ void CModelViewer::ShowContextMenu(CModelTreeItem* data, QPoint pt)
 			{
 				menu.addAction("Select", this, SLOT(OnSelectPart()));
 				menu.addAction("Hide", this, SLOT(OnHidePart()));
+				menu.addAction("Select elements", this, SLOT(OnSelectPartElements()));
 			}
 			else
 				menu.addAction("Show", this, SLOT(OnShowPart()));
