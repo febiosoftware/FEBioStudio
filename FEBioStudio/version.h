cb87ff1549bbd3a10a0c40eb0504d156e1d8ef45
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2020-10-07 09:46:24 -0600

    Updated version to 1.0.3. Update release notes.

--- FEBioStudio/version.h
+++ FEBioStudio/version.h
@@ -30,7 +30,7 @@ SOFTWARE.*/
 // VERSION INFORMATION
 #define VERSION			1
 #define SUBVERSION		0
-#define SUBSUBVERSION	2
+#define SUBSUBVERSION	3
 
 //-----------------------------------------------------------------------------
 // FSPRJ FILE VERSION
