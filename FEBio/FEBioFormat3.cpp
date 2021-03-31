f51df04e76cfc4c6fa74b50cec6280044175f54e
Gerard Ateshian <ateshian@columbia.edu>
2021-03-31 18:09:53 -0400

    Fixed bug in newly committed FEBioFormat3.cpp for direction of prescribed vectorial BCs.

--- FEBio/FEBioFormat3.cpp
+++ FEBio/FEBioFormat3.cpp
@@ -1402,21 +1402,21 @@ void FEBioFormat3::ParseBCPrescribed(FEStep* pstep, XMLTag& tag)
 
 	// make a new boundary condition
 	FEPrescribedDOF* pbc = 0;
-    if (bc=="x") pbc = new FEPrescribedDisplacement (&fem, pg, 1, 1, pstep->GetID());
-    else if (bc=="y") pbc = new FEPrescribedDisplacement (&fem, pg, 2, 1, pstep->GetID());
-    else if (bc=="z") pbc = new FEPrescribedDisplacement (&fem, pg, 3, 1, pstep->GetID());
+    if (bc=="x") pbc = new FEPrescribedDisplacement (&fem, pg, 0, 1, pstep->GetID());
+    else if (bc=="y") pbc = new FEPrescribedDisplacement (&fem, pg, 1, 1, pstep->GetID());
+    else if (bc=="z") pbc = new FEPrescribedDisplacement (&fem, pg, 2, 1, pstep->GetID());
     else if (bc=="T") pbc = new FEPrescribedTemperature  (&fem, pg, 1, pstep->GetID());
     else if (bc=="p") pbc = new FEPrescribedFluidPressure(&fem, pg, 1, pstep->GetID());
-    else if (bc=="vx") pbc = new FEPrescribedFluidVelocity(&fem, pg, 1, 1, pstep->GetID());
-    else if (bc=="vy") pbc = new FEPrescribedFluidVelocity(&fem, pg, 2, 1, pstep->GetID());
-    else if (bc=="vz") pbc = new FEPrescribedFluidVelocity(&fem, pg, 3, 1, pstep->GetID());
+    else if (bc=="vx") pbc = new FEPrescribedFluidVelocity(&fem, pg, 0, 1, pstep->GetID());
+    else if (bc=="vy") pbc = new FEPrescribedFluidVelocity(&fem, pg, 1, 1, pstep->GetID());
+    else if (bc=="vz") pbc = new FEPrescribedFluidVelocity(&fem, pg, 2, 1, pstep->GetID());
     else if (bc=="ef") pbc = new FEPrescribedFluidDilatation(&fem, pg, 1, pstep->GetID());
-    else if (bc=="sx") pbc = new FEPrescribedShellDisplacement(&fem, pg, 1, 1, pstep->GetID());
-    else if (bc=="sy") pbc = new FEPrescribedShellDisplacement(&fem, pg, 2, 1, pstep->GetID());
-    else if (bc=="sz") pbc = new FEPrescribedShellDisplacement(&fem, pg, 3, 1, pstep->GetID());
-    else if (bc=="u") pbc = new FEPrescribedRotation(&fem, pg, 1, 1, pstep->GetID());
-    else if (bc=="v") pbc = new FEPrescribedRotation(&fem, pg, 2, 1, pstep->GetID());
-    else if (bc=="w") pbc = new FEPrescribedRotation(&fem, pg, 3, 1, pstep->GetID());
+    else if (bc=="sx") pbc = new FEPrescribedShellDisplacement(&fem, pg, 0, 1, pstep->GetID());
+    else if (bc=="sy") pbc = new FEPrescribedShellDisplacement(&fem, pg, 1, 1, pstep->GetID());
+    else if (bc=="sz") pbc = new FEPrescribedShellDisplacement(&fem, pg, 2, 1, pstep->GetID());
+    else if (bc=="u") pbc = new FEPrescribedRotation(&fem, pg, 0, 1, pstep->GetID());
+    else if (bc=="v") pbc = new FEPrescribedRotation(&fem, pg, 1, 1, pstep->GetID());
+    else if (bc=="w") pbc = new FEPrescribedRotation(&fem, pg, 2, 1, pstep->GetID());
     else if (bc.compare(0,1,"c") == 0) {
         int isol;
         sscanf(bc.substr(1).c_str(),"%d",&isol);
