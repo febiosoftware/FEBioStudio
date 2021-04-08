b93bcc3b038321f9b758262301e0f830cc4de618
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-04-08 14:05:50 -0600

    Fixed performance issue when converting editable mesh to editable surface.

--- GeomLib/GSurfaceMeshObject.cpp
+++ GeomLib/GSurfaceMeshObject.cpp
@@ -30,6 +30,7 @@ SOFTWARE.*/
 #include <MeshTools/FEModifier.h>
 #include <MeshLib/FECurveMesh.h>
 #include <MeshTools/GLMesh.h>
+#include <MeshLib/FENodeEdgeList.h>
 #include "GOCCObject.h"
 
 GSurfaceMeshObject::GSurfaceMeshObject(FESurfaceMesh* pm) : GObject(GSURFACEMESH_OBJECT), m_surfmesh(pm)
@@ -172,13 +173,28 @@ GSurfaceMeshObject::GSurfaceMeshObject(GObject* po) : GObject(GSURFACEMESH_OBJEC
 		m_surfmesh->UpdateBoundingBox();
 
 		// copy edges
+		// Build a node-edge tabel
+		FENodeEdgeList NEL(m_surfmesh);
 		m_surfmesh->BuildEdges();
 		for (int i = 0; i < pm->Edges(); ++i)
 		{
 			FEEdge& src = pm->Edge(i);
 			int n0 = pm->Node(src.n[0]).m_ntag;
 			int n1 = pm->Node(src.n[1]).m_ntag;
-			FEEdge* pe = m_surfmesh->FindEdge(n0, n1);
+
+			FEEdge* pe = nullptr;
+			const std::vector<int>& el = NEL.EdgeIndexList(n0);
+			for (int k = 0; k < el.size(); ++k)
+			{
+				FEEdge& e = m_surfmesh->Edge(el[k]);
+				if (((e.n[0] == n0) && (e.n[1] == n1)) ||
+					((e.n[0] == n1) && (e.n[1] == n0)))
+				{
+					FEEdge* pe = &e;
+					break;
+				}
+			}
+
 			assert(pe);
 			if (pe)
 			{
