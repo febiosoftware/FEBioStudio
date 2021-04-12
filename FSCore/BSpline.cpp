67e49a4a4be96204f35af42f321a6e6f44c97a2e
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-04-12 09:26:51 -0600

    Updated VS2017 project.

--- FSCore/BSpline.cpp
+++ FSCore/BSpline.cpp
@@ -24,7 +24,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.*/
 
-
+#include "stdafx.h"
 #include "BSpline.h"
 #include <MathLib/math3d.h>
 #include <limits>
