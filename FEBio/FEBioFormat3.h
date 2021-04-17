0ceb87fc7609008d755789480c9953c30f819ad8
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-04-17 07:36:14 -0600

    Fixed issue with importing fluid rotational velocity boundary condition from febio3 file.

--- FEBio/FEBioFormat3.h
+++ FEBio/FEBioFormat3.h
@@ -75,6 +75,7 @@ private:
 	void ParseBCFixed     (FEStep* pstep, XMLTag& tag);
 	void ParseBCPrescribed(FEStep* pstep, XMLTag& tag);
 	void ParseBCRigid     (FEStep* pstep, XMLTag& tag);
+	void ParseBCFluidRotationalVelocity(FEStep* pstep, XMLTag& tag);
 
 	// mesh data sections
 	bool ParseElementDataSection(XMLTag& tag);
