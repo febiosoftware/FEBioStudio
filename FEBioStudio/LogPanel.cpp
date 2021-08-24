7ea075dd1311b5a4b1d2e9b1febd41077b3ffa7f
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-08-24 12:53:19 -0600

    Fixed issue with scrolling output to end.

--- FEBioStudio/LogPanel.cpp
+++ FEBioStudio/LogPanel.cpp
@@ -130,9 +130,7 @@ void CLogPanel::AddText(const QString& txt, int n)
 	}
 	cursor.setCharFormat(ui->defaultTextCharFormat);
 	cursor.movePosition(QTextCursor::End);
-
-	// TODO: for some reason this crashes sometimes. 
-//	ui->txt[n]->setTextCursor(cursor);
+	ui->txt[n]->ensureCursorVisible();
 }
 
 void CLogPanel::parseEscapeSequence(int attribute, QListIterator< QString > & i, QTextCharFormat & textCharFormat, QTextCharFormat const & defaultTextCharFormat)
