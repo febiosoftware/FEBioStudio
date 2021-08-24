6cc7e37cdb734efa21a04171908ef1dfbaffc4ad
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-08-24 13:56:07 -0600

    Added some macro to eliminate the Qt warning related to on_actionLayerInfo_triggered().

--- FEBioStudio/MainWindow.h
+++ FEBioStudio/MainWindow.h
@@ -401,8 +401,9 @@ public slots:
 	void on_actionFEBioStop_triggered();
 	void on_actionFEBioOptimize_triggered();
 	void on_actionOptions_triggered();
+#ifdef _DEBUG
 	void on_actionLayerInfo_triggered();
-
+#endif
 	// Post menu actions
 	void on_actionPlaneCut_triggered();
 	void on_actionMirrorPlane_triggered();
