9d41513aaa43ee303febc273e8d324059c62ab1a
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-12-03 13:45:52 -0700

    Added support for exporting to STL from post side.

--- FEBioStudio/menuFile.cpp
+++ FEBioStudio/menuFile.cpp
@@ -118,6 +118,7 @@ SOFTWARE.*/
 #include <PostLib/VolRender.h>
 #include <PostLib/VolumeRender2.h>
 #include <sstream>
+#include "PostObject.h"
 
 using std::stringstream;
 
@@ -534,7 +535,8 @@ void CMainWindow::ExportPostGeometry()
 		//		<< "FEBio files (*.feb)"
 		//		<< "ASCII files (*.*)"
 		//		<< "VRML files (*.wrl)"
-		<< "LSDYNA Keyword (*.k)";
+		<< "LSDYNA Keyword (*.k)"
+		<< "STL file (*.stl)";
 	//		<< "BYU files(*.byu)"
 	//		<< "NIKE3D files (*.n)"
 	//		<< "VTK files (*.vtk)"
@@ -626,6 +628,14 @@ void CMainWindow::ExportPostGeometry()
 			w.m_bnode = dlg.m_bnode;
 			bret = w.Save(fem, doc->GetActiveState(), szfilename);
 		}
+	}
+	break;
+	case 2:
+	{
+		// We need a dummy project
+		FEProject prj;
+		FESTLExport stl(prj);
+		bret = stl.Write(szfilename, doc->GetPostObject());
 	}
 	break;
 	/*	case 5:
