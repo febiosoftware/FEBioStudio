1b3fcdbfbdd38a471e35f96f554f4f91bbc682f5
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2021-12-15 08:52:02 -0700

    FEBio post export now exports current mesh.

--- PostLib/FEFEBioExport.cpp
+++ PostLib/FEFEBioExport.cpp
@@ -62,8 +62,8 @@ bool FEFEBioExport::Save(FEPostModel& fem, const char* szfile)
 {
 	int nmat = fem.Materials();
 
-	FEPostMesh* pm = fem.GetFEMesh(0);
 	FEState* pst = fem.CurrentState();
+	FEPostMesh* pm = pst->GetFEMesh();
 	int NE = pm->Elements();
 
 	XMLWriter xml;
