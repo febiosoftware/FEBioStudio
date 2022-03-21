cc03f3e06adb6cc85c005d60abf5f2e95b7d834e
Gerard Ateshian <ateshian@columbia.edu>
2022-03-20 20:55:29 -0400

    Fixed bug to properly save plot_level to feb file for modules other than structural mechanics.

--- FEBio/FEBioExport3.cpp
+++ FEBio/FEBioExport3.cpp
@@ -1219,6 +1219,12 @@ void FEBioExport3::WriteBiphasicControlParams(FEAnalysisStep* pstep)
 		m_xml.close_branch();
 	}
 
+    if (ops.plot_level != 1)
+    {
+        const char* sz[] = { "PLOT_NEVER", "PLOT_MAJOR_ITRS", "PLOT_MINOR_ITRS", "PLOT_MUST_POINTS", "PLOT_FINAL", "PLOT_AUGMENTATIONS", "PLOT_STEP_FINAL" };
+        m_xml.add_leaf("plot_level", sz[ops.plot_level]);
+    }
+
 	if (ops.plot_stride != 1)
 	{
 		m_xml.add_leaf("plot_stride", ops.plot_stride);
@@ -1280,6 +1286,12 @@ void FEBioExport3::WriteBiphasicSoluteControlParams(FEAnalysisStep* pstep)
 		m_xml.close_branch();
 	}
 
+    if (ops.plot_level != 1)
+    {
+        const char* sz[] = { "PLOT_NEVER", "PLOT_MAJOR_ITRS", "PLOT_MINOR_ITRS", "PLOT_MUST_POINTS", "PLOT_FINAL", "PLOT_AUGMENTATIONS", "PLOT_STEP_FINAL" };
+        m_xml.add_leaf("plot_level", sz[ops.plot_level]);
+    }
+
 	if (ops.plot_stride != 1)
 	{
 		m_xml.add_leaf("plot_stride", ops.plot_stride);
@@ -1339,6 +1351,12 @@ void FEBioExport3::WriteFluidControlParams(FEAnalysisStep* pstep)
 		}
 		m_xml.close_branch();
 	}
+
+    if (ops.plot_level != 1)
+    {
+        const char* sz[] = { "PLOT_NEVER", "PLOT_MAJOR_ITRS", "PLOT_MINOR_ITRS", "PLOT_MUST_POINTS", "PLOT_FINAL", "PLOT_AUGMENTATIONS", "PLOT_STEP_FINAL" };
+        m_xml.add_leaf("plot_level", sz[ops.plot_level]);
+    }
 
 	if (ops.plot_stride != 1)
 	{
@@ -1400,6 +1418,12 @@ void FEBioExport3::WriteFluidFSIControlParams(FEAnalysisStep* pstep)
 		m_xml.close_branch();
 	}
 
+    if (ops.plot_level != 1)
+    {
+        const char* sz[] = { "PLOT_NEVER", "PLOT_MAJOR_ITRS", "PLOT_MINOR_ITRS", "PLOT_MUST_POINTS", "PLOT_FINAL", "PLOT_AUGMENTATIONS", "PLOT_STEP_FINAL" };
+        m_xml.add_leaf("plot_level", sz[ops.plot_level]);
+    }
+
 	if (ops.plot_stride != 1)
 	{
 		m_xml.add_leaf("plot_stride", ops.plot_stride);
@@ -1453,6 +1477,12 @@ void FEBioExport3::WriteReactionDiffusionControlParams(FEAnalysisStep* pstep)
 		}
 		m_xml.close_branch();
 	}
+
+    if (ops.plot_level != 1)
+    {
+        const char* sz[] = { "PLOT_NEVER", "PLOT_MAJOR_ITRS", "PLOT_MINOR_ITRS", "PLOT_MUST_POINTS", "PLOT_FINAL", "PLOT_AUGMENTATIONS", "PLOT_STEP_FINAL" };
+        m_xml.add_leaf("plot_level", sz[ops.plot_level]);
+    }
 
 	if (ops.plot_stride != 1)
 	{
