5d33520d54f028961ab8433c10a18d754be8c647
michaelrossherron <michaelrossherron@gmail.com>
2022-10-25 17:24:50 -0600

    Began exposing FSModel, GModel, and GNode classes

--- PyLib/pyFBSModel.cpp
+++ PyLib/pyFBSModel.cpp
@@ -37,7 +37,9 @@ SOFTWARE.*/
 #include <FEBioStudio/Commands.h>
 #include <FEBioLink/FEBioClass.h>
 #include <MeshTools/GModel.h>
+#include <GeomLib/GItem.h>
 #include <FEBio/FEBioExport4.h>
+#include <MeshTools/FEModel.h>
 
 CModelDocument* GetActiveDocument()
 {
@@ -46,7 +48,7 @@ CModelDocument* GetActiveDocument()
 	return doc;
 }
 
-FSModel* GetActiveModel()
+FSModel* GetActiveFSModel()
 {
 	CModelDocument* doc = GetActiveDocument();
 	return doc->GetFSModel();
@@ -85,7 +87,7 @@ GObject* CreateBox(std::string& name, vec3d pos, double width, double height, do
 
 GMaterial* AddMaterial(std::string& name, std::string& type)
 {
-	FSModel* fem = GetActiveModel();
+	FSModel* fem = GetActiveFSModel();
 	FSMaterial* pm = FEBio::CreateMaterial(type, fem);
 	if (pm)
 	{
@@ -100,7 +102,7 @@ GMaterial* AddMaterial(std::string& name, std::string& type)
 
 FSStep* AddStep(std::string& name)
 {
-	FSModel* fem = GetActiveModel();
+	FSModel* fem = GetActiveFSModel();
 	FSStep* step = FEBio::CreateStep("solid", fem);
 	if (step) fem->AddStep(step);
 	return step;
@@ -117,6 +119,112 @@ bool ExportFEB(std::string& fileName)
 void init_FBSModel(pybind11::module& m)
 {
 	pybind11::module mdl = m.def_submodule("mdl", "Module used to interact with an FEBio model.");
+
+
+    ///////////////// FSModel /////////////////
+    mdl.def("GetActiveFSModel", &GetActiveFSModel);
+
+    pybind11::class_<FSModel, std::unique_ptr<FSModel, pybind11::nodelete>>(mdl, "FSModel")
+        .def("Clear", &FSModel::Clear)
+        .def("Purge", &FSModel::Purge)
+
+        // --- functions to delete all components ---
+        .def("DeleteAllMaterials", &FSModel::DeleteAllMaterials)
+        .def("DeleteAllBC", &FSModel::DeleteAllBC)
+        .def("DeleteAllLoads", &FSModel::DeleteAllLoads)
+        .def("DeleteAllIC", &FSModel::DeleteAllIC)
+        .def("DeleteAllContact", &FSModel::DeleteAllContact)
+        .def("DeleteAllConstraints", &FSModel::DeleteAllRigidBCs)
+        .def("DeleteAllRigidICs", &FSModel::DeleteAllRigidICs)
+        .def("DeleteAllRigidLoads", &FSModel::DeleteAllRigidLoads)
+        .def("DeleteAllRigidConnectors", &FSModel::DeleteAllRigidConnectors)
+        .def("DeleteAllSteps", &FSModel::DeleteAllSteps)
+        .def("DeleteAllLoadControllers", &FSModel::DeleteAllLoadControllers)
+        .def("DeleteAllMeshDataGenerators", &FSModel::DeleteAllMeshDataGenerators)
+        
+        .def("ClearSelections", &FSModel::ClearSelections)
+        .def("New", &FSModel::New)
+        .def("GetModel", &FSModel::GetModel)
+
+        // --- material functions ---
+        .def("GetMaterial", &FSModel::GetMaterial)
+        .def("AddMaterial", &FSModel::AddMaterial)
+        .def("ReplaceMaterial", &FSModel::ReplaceMaterial)
+        .def("CanDeleteMaterial", &FSModel::CanDeleteMaterial)
+        .def("DeleteMaterial", &FSModel::DeleteMaterial)
+        .def("InsertMaterial", &FSModel::InsertMaterial)
+        .def("Materials", &FSModel::Materials)
+        .def("GetMaterialFromID", &FSModel::GetMaterialFromID)
+        .def("FindMaterial", &FSModel::FindMaterial)
+        .def("GetRigidConnectorFromID", &FSModel::GetRigidConnectorFromID)
+        .def("GetModel", &FSModel::GetModel)
+        
+        // --- Analysis steps ---
+        .def("Steps", &FSModel::Steps)
+        .def("GetStep", &FSModel::GetStep)
+        .def("FindStep", &FSModel::FindStep)
+        .def("GetStepIndex", &FSModel::GetStepIndex)
+        .def("AddStep", &FSModel::AddStep)
+        .def("DeleteStep", &FSModel::DeleteStep)
+        .def("InsertStep", &FSModel::InsertStep)
+        .def("SwapSteps", &FSModel::SwapSteps)
+        .def("ReplaceStep", &FSModel::ReplaceStep)
+        .def("AssignComponentToStep", &FSModel::AssignComponentToStep)
+        ;
+    ///////////////// FSModel /////////////////
+
+    ///////////////// GModel /////////////////
+    mdl.def("GetActiveGModel", &GetActiveGModel);
+
+    pybind11::class_<GModel, std::unique_ptr<GModel, pybind11::nodelete>>(mdl, "GModel")
+        .def("Clear", &GModel::Clear)
+        .def("ClearGroups", &GModel::ClearGroups)
+        .def("ClearDiscrete", &GModel::ClearDiscrete)
+
+        // --- Object functions ---
+        .def("Objects", &GModel::Objects)
+        .def("Object", &GModel::Object)
+        .def("FindObject", static_cast<GObject* (GModel::*)(int)>(&GModel::FindObject))
+        .def("FindObject", static_cast<GObject* (GModel::*)(const string&)>(&GModel::FindObject))
+        .def("FindObjectIndex", &GModel::FindObjectIndex)
+        .def("ReplaceObject", static_cast<void (GModel::*)(int, GObject*)>(&GModel::ReplaceObject))
+        .def("ReplaceObject", static_cast<void (GModel::*)(GObject*, GObject*)>(&GModel::ReplaceObject))
+        .def("AddObject", &GModel::AddObject)
+        .def("RemoveObject", &GModel::RemoveObject)
+        .def("InsertObject", &GModel::InsertObject)
+
+        // --- part functions ---
+        .def("Parts", &GModel::Parts)
+        .def("Part", &GModel::Part)
+        .def("FindPart", &GModel::FindPart)
+
+        // --- surface functions ---
+        .def("Surfaces", &GModel::Surfaces)
+        .def("Surface", &GModel::Surface)
+        .def("FindSurface", &GModel::FindSurface)
+
+        // --- edge functions ---
+        .def("Edges", &GModel::Edges)
+        .def("Edge", &GModel::Edge)
+        .def("FindEdge", &GModel::FindEdge)
+        .def("FindEdgeFromName", &GModel::FindEdgeFromName)
+
+        // --- node functions ---
+        .def("Nodes", &GModel::Nodes)
+        .def("Node", &GModel::Node)
+        .def("FindNode", &GModel::FindNode)
+        ;
+    ///////////////// GModel /////////////////
+
+    ///////////////// GNode /////////////////
+    pybind11::class_<GNode, std::unique_ptr<GNode, pybind11::nodelete>>(mdl, "GNode")
+        .def("Type", &GNode::Type)
+        .def("SetType", &GNode::SetType)
+        .def("LocalPosition", static_cast<vec3d& (GNode::*)()>(&GNode::LocalPosition))
+        .def("Position", &GNode::Position)
+        .def("MakeRequired", &GNode::MakeRequired)
+        ;
+    ///////////////// GNode /////////////////
 
 	mdl.def("CreateBox", CreateBox);
 	mdl.def("AddMaterial", AddMaterial);
@@ -138,9 +246,11 @@ void init_FBSModel(pybind11::module& m)
 		.def("AssignMaterial", [](GObject* po, GMaterial* pm) {
 			for (int i = 0; i < po->Parts(); ++i) po->Part(i)->SetMaterialID(pm->GetID());
 		})
-		.def("BuildMesh", [](GObject* po) {
-			po->BuildMesh();
-			});
+        .def("BuildMesh", &GObject::BuildMesh);
+			// });
+		// .def("BuildMesh", [](GObject* po) {
+		// 	po->BuildMesh();
+		// 	});
 
 }
 #else
