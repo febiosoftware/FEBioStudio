6c282332d17488d88915b9168128afcc56340571
John Haynes <tundranerd@gmail.com>
2022-11-27 19:50:25 -0700

    WIP - CI for febiostudio2

---
+++ ci/linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+LIB_DIR="/usr/lib/x86_64-linux-gnu"
+cmake -L . -B cmbuild \
+	-DQt_Root=$LIB_DIR \
+	-DFFMPEG_LIB_DIR=$LIB_DIR \
+   	-DUSE_FFMPEG=ON \
+	-DTETGEN_LIB_DIR=$LIB_DIR \
+	-DUSE_TETGEN=ON \
+	-DUSE_MMG=ON \
+	-DSSH_LIB_DIR=$LIB_DIR \
+	-DUSE_SSH=ON \
+	-DUSE_SSL=ON \
+	-DNETGEN_LIB_DIR=/usr/local \
+	-DUSE_NETGEN=ON \
+	-DUSE_ITK=ON \
+
+
+pushd cmbuild
+make -j $(nproc)
+popd
