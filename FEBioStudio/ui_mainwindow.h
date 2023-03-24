a33d1ad3ca1a5276375288d47e29d972c90e6842
SteveMaas1978 <50155781+SteveMaas1978@users.noreply.github.com>
2023-03-24 11:05:33 -0600

    Additional updates to FEBio Studio user manual.

--- FEBioStudio/ui_mainwindow.h
+++ FEBioStudio/ui_mainwindow.h
@@ -1233,7 +1233,7 @@ public:
 		menuWindows->addAction(dock8->toggleViewAction());
 		m_wnd->tabifyDockWidget(dock4, dock8);
 
-        QDockWidget* dock9 = new QDockWidget("Image Settings", m_wnd); dock8->setObjectName("dockImageSettings");
+		QDockWidget* dock9 = new QDockWidget("3D Image Settings", m_wnd); dock8->setObjectName("dockImageSettings");
 		imageSettingsPanel = new ::CImageSettingsPanel(wnd, dock9);
 		dock9->setWidget(imageSettingsPanel);
 		menuWindows->addAction(dock9->toggleViewAction());
