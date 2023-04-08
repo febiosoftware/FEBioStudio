f58c063eee2803a9d7eac055f8bab927ff1404e3
John Haynes <tundranerd@gmail.com>
2023-04-08 13:28:53 -0600

    Pull sdk from s3

--- ci/linux/febio-sdk.sh
+++ ci/linux/febio-sdk.sh
@@ -2,25 +2,8 @@
 set -o errexit
 set -o verbose
 
-# sudo apt-get install jq -y
-LOCATE_ARCHIVE_URL="https://api.github.com/repos/febiosoftware/FEBio/actions/artifacts?name=febio4-sdk-Linux-X64&per_page=1"
-
-get_archive_url() {
-	local locate_archive_url=$1
-	echo $(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" $locate_archive_url | jq -r .artifacts[0].archive_download_url)
-}
-
-get_archive() {
-	local archive_url=$1
-	local output_file=$2
-	curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GH_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" $archive_url --output $output_file
-}
-
-main() {
-	local archive_url=$(get_archive_url $LOCATE_ARCHIVE_URL)
-	local output_file="febio4-sdk-Linux-X64.zip"
-	get_archive "$archive_url" $output_file
-	unzip "$output_file" -d "febio-sdk"
-}
-
-main
+BUCKET=${BUCKET:-febio-packages}
+PACKAGE_NAME=${PACKAGE_NAME-febio}
+SDK_S3_URI="s3://$BUCKET/$PACKAGE_NAME/$FEBIO_SDK_BRANCH/$OS/$PACKAGE"
+aws s3 cp $SDK_S3_URI .
+tar xzf $PACKAGE
