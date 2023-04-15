0fe1355022ee21403cbb5cb80f8ed08c1ba6dfbf
John Haynes <tundranerd@gmail.com>
2023-04-15 16:03:06 -0600

    Use ci/common

--- ci/linux/febio-sdk.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -o errexit
-set -o verbose
-
-BUCKET=${BUCKET:-febio-packages}
-PACKAGE_NAME=${PACKAGE_NAME-febio}
-SDK_S3_URI="s3://$BUCKET/$PACKAGE_NAME/$FEBIO_SDK_BRANCH/$OS/$PACKAGE"
-aws s3 cp $SDK_S3_URI .
-tar xzf $PACKAGE
