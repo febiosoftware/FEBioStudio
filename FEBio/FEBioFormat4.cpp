4d69c2129c2e442619955cafd07cc84776e1b1d4
SteveMaas1978 <steve.maas@utah.edu>
2023-08-17 13:30:24 -0600

    Node IDs are now stored when reading feb file.

--- FEBio/FEBioFormat4.cpp
+++ FEBio/FEBioFormat4.cpp
@@ -593,6 +593,7 @@ void FEBioFormat4::ParseGeometryNodes(FEBioInputModel::Part* part, XMLTag& tag)
 		FEBioInputModel::NODE& nd = nodes[i];
 		FSNode& node = mesh.Node(N0 + i);
 		node.m_ntag = nd.id;
+		node.m_nid = nd.id;
 		node.r = nd.r;
 	}
 
