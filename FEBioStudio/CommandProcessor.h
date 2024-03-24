bedc5cf15c86f685cba113b3aa50951ea7543cea
SteveMaas1978 <steve.maas@utah.edu>
2024-03-24 08:50:04 -0600

    Moved CommandProcessor class to its own files.

---
+++ FEBioStudio/CommandProcessor.h
@@ -0,0 +1,106 @@
+/*This file is part of the FEBio Studio source code and is licensed under the MIT license
+listed below.
+
+See Copyright-FEBio-Studio.txt for details.
+
+Copyright (c) 2021 University of Utah, The Trustees of Columbia University in
+the City of New York, and others.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+#pragma once
+#include <QString>
+#include <QStringList>
+#include <FSCore/color.h>
+#include <vector>
+
+class CMainWindow;
+
+class CommandProcessor
+{
+private:
+	struct CCommandDescriptor
+	{
+		QString name;	// name of command
+		bool(CommandProcessor::* f)(QStringList);
+		QString brief;	// brief description
+	};
+
+public:
+	CommandProcessor(CMainWindow* wnd);
+	QString GetCommandOutput() { return m_output; }
+	bool ProcessCommandLine(QString cmdLine);
+	bool RunCommand(QString cmd, QStringList ops);
+	QStringList ParseCommandLine(QString cmd);
+
+public: // command functions
+	bool cmd_addbc  (QStringList ops);
+	bool cmd_addbl  (QStringList ops);
+	bool cmd_addci  (QStringList ops);
+	bool cmd_addmat (QStringList ops);
+	bool cmd_addnl  (QStringList ops);
+	bool cmd_addsl  (QStringList ops);
+	bool cmd_addstep(QStringList ops);
+	bool cmd_anim   (QStringList ops);
+	bool cmd_assign (QStringList ops);
+	bool cmd_bgcol  (QStringList ops);
+	bool cmd_bgcol1 (QStringList ops);
+	bool cmd_bgcol2 (QStringList ops);
+	bool cmd_bgstyle(QStringList ops);
+	bool cmd_close  (QStringList ops);
+	bool cmd_cmd    (QStringList ops);
+	bool cmd_create (QStringList ops);
+	bool cmd_exit   (QStringList ops);
+	bool cmd_export (QStringList ops);
+	bool cmd_expgeo (QStringList ops);
+	bool cmd_fgcol  (QStringList ops);
+	bool cmd_first  (QStringList ops);
+	bool cmd_grid   (QStringList ops);
+	bool cmd_help   (QStringList ops);
+	bool cmd_import (QStringList ops);
+	bool cmd_job    (QStringList ops);
+	bool cmd_genmesh(QStringList ops);
+	bool cmd_last   (QStringList ops);
+	bool cmd_new    (QStringList ops);
+	bool cmd_next   (QStringList ops);
+	bool cmd_open   (QStringList ops);
+	bool cmd_prev   (QStringList ops);
+	bool cmd_reset  (QStringList ops);
+	bool cmd_save   (QStringList ops);
+	bool cmd_selpart(QStringList ops);
+	bool cmd_selsurf(QStringList ops);
+
+private: // error messages
+	bool Error(const QString& msg) { m_output = msg; return false; }
+	bool Success(const QString& msg) { m_output = msg; return true; }
+
+	bool NoActiveDoc() { return Error("No model active."); }
+	bool GLViewIsNull() { return Error("Graphics View not available."); }
+	bool InvalidArgsCount() { return Error("Invalid number of arguments."); }
+
+private:
+	bool ValidateArgs(const QStringList& ops, int minargs = -1, int maxargs = -1);
+	bool ValidateArgs(const QStringList& ops, const std::vector<int>& validSizes);
+	bool CmdToColor(QStringList ops, GLColor& c);
+	bool RunCommandFile(QString cmdFile, QStringList ops);
+
+private:
+	CMainWindow* m_wnd;
+	QString m_output;
+	std::vector<CCommandDescriptor> m_cmds;
+};
