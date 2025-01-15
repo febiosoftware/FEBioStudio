63f84bba8a2e7e92e18010fe8f89082e4dc23593
michaelrossherron <michaelrossherron@gmail.com>
2025-01-15 15:25:28 -0700

    Updated ci scripts to allow for dev releases on other branches [skip ci]

--- ci/Windows/scp-to-repo.sh
+++ ci/Windows/scp-to-repo.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-scp cmbuild/bin/Release/* repo:~/update2/FEBioStudio2Dev/Windows/stage/bin
+
+REMOTE_PATH="update2/FEBioStudio2Dev/Windows/stage"
+if [ $# == 1 ] && [ "$1" != "develop" ]; then
+    REMOTE_PATH="update2/FEBioStudio2Dev/branches/$1/Windows/stage"
+fi
+
+scp cmbuild/bin/Release/* repo:~/$REMOTE_PATH/bin
