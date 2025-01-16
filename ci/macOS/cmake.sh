e96d0e4c2237bb8a2521c93579aa1a0dfcefaf5c
michaelrossherron <michaelrossherron@gmail.com>
2025-01-16 11:41:45 -0700

    Updated CMakeLists and macOS cmake CI script to find omp.h

--- ci/macOS/cmake.sh
+++ ci/macOS/cmake.sh
@@ -12,7 +12,10 @@ run_cmake() {
 		-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
 		-DCMAKE_OSX_ARCHITECTURES=x86_64 \
 		-DBUILD_UPDATER=ON \
-		-DHOMEBREW=ON
+		-DHOMEBREW=ON \
+        -DUSE_MKL_OMP=ON \
+        -DMKL_OMP=/opt/intel/oneapi/compiler/latest/mac/compiler/lib/libiomp5.dylib \
+        -DOMP_INC=/Users/gitRunner/local/x86_64/homebrew/opt/libomp/include
 }
 
 
