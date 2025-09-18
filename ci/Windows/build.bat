390d247e6e7414369b78823c60a74f689a86f6dc
michaelrossherron <michaelrossherron@gmail.com>
2025-09-18 16:49:31 -0600

    CMakeLists no longer sets CMAKE_FILES_DIRECTORY. Fixes cmake configuration issue.

--- ci/Windows/build.bat
+++ ci/Windows/build.bat
@@ -1,9 +1,6 @@
 call "%VS2019INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat"
 
-cmake -version
 set Qt_Root="c:/usr/local/Qt/6.7.3/msvc2019_64"
-:: TODO: Cmake requires 6 runs to generate correctly
-for /l %%a in (1, 1, 6) do (
 cmake -L . -B cmbuild ^
   -DQt_Root=%Qt_Root% ^
   -DFEBio_SDK=febio4-sdk ^
@@ -18,7 +15,7 @@ cmake -L . -B cmbuild ^
   -DMODEL_REPO=ON ^
   -DUSE_ITK=ON ^
   -DBUILD_UPDATER=ON
-)
+
 cd cmbuild
 msbuild /v:m /P:Configuration=Release /clp:ErrorsOnly /m:%NUMBER_OF_PROCESSORS% ALL_BUILD.vcxproj
 cd ..
