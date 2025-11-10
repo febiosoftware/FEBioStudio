57a6dbd3212f9a8ba6b0bee8082f0dd8ed7a8184
SteveMaas1978 <steve.maas@utah.edu>
2025-11-10 10:42:23 -0700

    Fixed include statement to fix linux build.

--- FEBioStudio/GLView.h
+++ FEBioStudio/GLView.h
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
 #pragma once
-#include <rhiLib/rhiSceneView.h>
+#include <RHILib/rhiSceneView.h>
 #include <QNativeGestureEvent>
 #include <GLLib/GLCamera.h>
 #include <FSCore/ColorMap.h>
