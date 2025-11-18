c07a652bd3c6f5ca61a71c88ed381e726a9aae5c
michaelrossherron <michaelrossherron@gmail.com>
2025-11-18 15:14:55 -0700

    macOS will now build a universal standalone Python module [skip ci]

--- ci/macOS/build.sh
+++ ci/macOS/build.sh
@@ -23,8 +23,12 @@ main() {
     git clone --depth 1 https://github.com/febiosoftware/FEBio.git
     cmake -L . -B cmbuild \
         -DFEBioDir=FEBio \
+        -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
         -DOMP_INC=/Users/gitRunner/local/x86_64/homebrew/opt/libomp/include \
-        -DCMAKE_OSX_ARCHITECTURES=x86_64
+        -DTETGEN_INC=/Users/gitRunner/local/universal/include \
+        -DTETGEN_LIB_DIR=/Users/gitRunner/local/universal/lib \
+        -DZLIB_LIBRARY_RELEASE=/Users/gitRunner/local/universal/lib/libz.a
+
     pushd cmbuild
     make -j $(sysctl -n hw.ncpu)
     popd
