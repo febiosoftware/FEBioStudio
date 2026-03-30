8eba32b69be5be708aa363fa20b038e852da3e7c
michaelrossherron <michaelrossherron@gmail.com>
2026-03-30 16:19:29 -0600

    Fixed issues with build script

--- ci/Linux/build.sh
+++ ci/Linux/build.sh
@@ -8,15 +8,15 @@ CURRENT_DIR=$(pwd)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CURRENT_DIR/febio4-sdk/lib:/usr/local/lib
 
 main() {
-    git clone https://github.com/google/googletest.git ~/googletest
-	pushd ~/googletest
-	cmake -S . -B build
-	pushd build
-	make install -j$(nproc)
-	popd
-	popd	
+    # git clone https://github.com/google/googletest.git ~/googletest
+	# pushd ~/googletest
+	# cmake -S . -B build
+	# pushd build
+	# make install -j$(nproc)
+	# popd
+	# popd	
 	
-	git clone https://github.com/pybind/pybind11.git /opt/pybind11
+	# git clone https://github.com/pybind/pybind11.git /opt/pybind11
 	
 	run_cmake
 	pushd cmbuild
