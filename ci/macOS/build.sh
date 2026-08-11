0299b9b1b9ae3296cf50e10c643d0c28d0ea1aea
michaelrossherron <michaelrossherron@gmail.com>
2026-08-11 11:40:41 -0600

    test

--- ci/macOS/build.sh
+++ ci/macOS/build.sh
@@ -4,25 +4,33 @@ RUN_POST_BUILD=${RUN_POST_BUILD:=true}
 
 security unlock-keychain -p "$MACOS_KEYCHAIN_PASSWORD" "$MACOS_KEYCHAIN"
 
-echo "List of keychains:"
-security list-keychains -d user
+# echo "List of keychains:"
+# security list-keychains -d user
 
-echo -e "\n\nDefault keychain:"
-security default-keychain -d user
+# echo -e "\n\nDefault keychain:"
+# security default-keychain -d user
 
-echo -e "\n\nDev ID Cert Auth:"
-security find-certificate -a -c "Developer ID Certification Authority" -Z
+# echo -e "\n\nDev ID Cert Auth:"
+# security find-certificate -a -c "Developer ID Certification Authority" -Z
 
-echo -e "\n\nApple root:"
-security find-certificate -a \
-  -c "Apple Root CA" \
-  -Z
+# echo -e "\n\nApple root:"
+# security find-certificate -a \
+#   -c "Apple Root CA" \
+#   -Z
 
-echo -e "\n\nSystem root store:"
+# echo -e "\n\nSystem root store:"
+# security find-certificate \
+#   -c "Apple Root CA" \
+#   -Z \
+#   /System/Library/Keychains/SystemRootCertificates.keychain || true
+
 security find-certificate \
-  -c "Apple Root CA" \
-  -Z \
-  /System/Library/Keychains/SystemRootCertificates.keychain || true
+  -c "Developer ID Application" \
+  -p "$MACOS_KEYCHAIN" > /tmp/developer-id.cer
+
+security verify-cert \
+  -c /tmp/developer-id.cer \
+  -p codesigning
 
 
 # cp /bin/echo ./sign-test
